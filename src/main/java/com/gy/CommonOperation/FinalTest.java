70848ebc35ea7c51c4448070e86e602935467de6
yantao <tao.yan@picahealth.com>
2017-11-26 18:18:37 +0800

    1.通过反射，获取对象中所有field的值，不用每次使用==null去做判断。

--- src/main/java/com/gy/CommonOperation/FinalTest.java
+++ src/main/java/com/gy/CommonOperation/FinalTest.java
@@ -35,12 +35,28 @@ public class FinalTest {
         one1 ="32";
         System.out.println(two1);
 
+
+        //通过调试断点，查看内存地址，得知：
+        /*
+            通过给String a ="xxx";,这样如果等号右侧值是一样的，则不管有多少个变量，他们的内存地址都是一样的。
+            通过String b = a+"xxx";每次计算都会新生成一个对象，不管等号右侧 a+"xxx"  的到的结果是否一样。
+            而通过 final String b = a +"xxx"; 每次生成的对象值如果相同，则他们指向同一片内存区域。
+         */
         String a = "hello2";
         final String b = "hello";
         String c = "hello";
 
         System.out.println(a==(b+2));
         System.out.println(a==(c+2));
+
+        String b1 = b+2;
+        String b2 = b+2;
+        String c1= c+2;
+        String c2= c+2;
+        System.out.println(a==b1);
+        System.out.println(a==b2);
+        System.out.println(a==c1);
+        System.out.println(a==c2);
 
 
     }
