f59f03b2c402accb985706bb52844d95e52a45cb
keason10 <729662970@qq.com>
2019-06-09 18:40:19 +0800

    CompletableFuture test，未完待续

---
+++ src/main/java/com/test/MyCompletableFutureTest.java
@@ -0,0 +1,59 @@
+package com.test;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.function.BiConsumer;
+import java.util.function.Function;
+
+public class MyCompletableFutureTest {
+    public static void main(String[] args) {
+        CompletableFuture completableFuture = CompletableFuture.supplyAsync(() -> {
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return 2;
+        });
+
+        //调用方法
+        try {
+            completableFuture.get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        //调用方法之后，会调用
+        completableFuture.whenComplete(new BiConsumer<Integer,Throwable>() {
+            @Override
+            public void accept(Integer o, Throwable o2) {
+                System.out.println(o);
+                System.out.println(o2);
+            }
+        });
+
+
+        System.out.println("\r\n 异常测试");
+        completableFuture = CompletableFuture.supplyAsync(() -> {
+                return 1 / 0;
+        });
+        try {
+            completableFuture.get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        //调用方法之后，会调用
+        completableFuture.whenComplete(new BiConsumer<Integer,Throwable>() {
+            @Override
+            public void accept(Integer o, Throwable o2) {
+                System.out.println(o);
+                System.out.println(o2);
+            }
+        });
+
+    }
+}
