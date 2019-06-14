2d12c3d69a837eb2cfbfcd0e70acca8970e2d21e
yantao <yant09@vanke.com>
2019-06-14 19:40:05 +0800

    group by partition by

--- src/main/java/com/lamba/MyOptional.java
+++ src/main/java/com/lamba/MyOptional.java
@@ -1,10 +1,12 @@
 package com.lamba;
 
+import com.alibaba.fastjson.JSON;
 import com.gy.jsonView.Student;
 import com.util.sort.SortUtil;
 import org.apache.commons.collections.CollectionUtils;
 
 import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -21,6 +23,7 @@ public class MyOptional {
                 .stream().filter(temp -> temp.getId() > 1).collect(Collectors.toList());
         myEntityList = Stream.of(new MyEntity(1, "1",Arrays.asList(new MyEntity(11,"11",null), new MyEntity(111,"111",null))),
                 new MyEntity(2, "2",Arrays.asList(new MyEntity(22,"22",null), new MyEntity(222,"222",null))),
+                new MyEntity(4, "2",Arrays.asList(new MyEntity(22,"22",null), new MyEntity(222,"222",null))),
                 new MyEntity(3, "3",Arrays.asList(new MyEntity(33,"33",null), new MyEntity(333,"333",null),new MyEntity(33,"33",null))))
                 .collect(Collectors.toList());
 
@@ -57,6 +60,26 @@ public class MyOptional {
         //如果对象为空则给个值
         myEntity = null;
         System.out.println(Optional.ofNullable(myEntity).map(i -> i.getName()).orElse("null"));
+
+
+
+        //从流中获取结果
+        System.out.println("从流中获取结果 \r\n");
+        IntSummaryStatistics intSummaryStatistics = myEntityList.stream().mapToInt(info -> info.getList().size()).summaryStatistics();
+        System.out.println(intSummaryStatistics.getAverage());
+        System.out.println(intSummaryStatistics.getCount());
+        System.out.println(intSummaryStatistics.getMax());
+        System.out.println(intSummaryStatistics.getMin());
+        System.out.println(intSummaryStatistics.getSum());
+
+        //数据分片
+        System.out.println("数据分片 partitioningBy \r\n");
+        Map<Boolean, List<MyEntity>> booleanListMap = myEntityList.stream().collect(Collectors.partitioningBy(info -> info.getId() > 2));
+        Stream.of(booleanListMap).peek(info-> System.out.println(JSON.toJSONString(info))).forEach(info -> System.out.println(JSON.toJSONString(info)));
+
+        System.out.println("数据分片 groupingBy \r\n");
+        Map maps = myEntityList.stream().collect(Collectors.groupingBy(myEntity1 -> myEntity1.getName()));
+        Stream.of(maps).peek(info-> System.out.println(JSON.toJSONString(info))).forEach(info -> System.out.println(JSON.toJSONString(info)));
 
     }
 }
